b648c6f57af849ec855dcd1c64c948338406aa66
Kyaw Min Lwin <kyawminlwinprogrammer@gmail.com>
2021-09-20 16:34:18 +0630

    Make it so the user can also play it with the mouse

--- 01 - JavaScript Drum Kit/script.js
+++ 01 - JavaScript Drum Kit/script.js
@@ -1,7 +1,17 @@
+let buttons = document.querySelectorAll('.key');
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        let keyCode = button.dataset.key;
+        let audio = document.querySelector(`audio[data-key = '${keyCode}']`);
+        audio.play();
+
+    })
+})
+
 document.addEventListener('keypress', (e) => {
     keyCode = findKey(e) - 32;
     let audio = document.querySelector(`audio[data-key = '${keyCode}']`);
-    audio.play();
+    audio ? audio.play() : null;
 })
 
 
